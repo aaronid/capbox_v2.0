613919a5cb9d4b423fa5bff41399a27b7aa58232
Gwenaelle Ferre <gwenaelle.ferre2016@campus-eni.fr>
2017-04-03 14:00:45 +0200

    Commit 03/04/2017
    Mise en place du repertory test CapBox

---
+++ reprise.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+	// mysql_connect("localhost","root","");
+	// mysql_select_db("capbox");
+	mysql_connect("mysql51-9.business", "capboxtest", "test00test");
+	mysql_select_db("capboxtest");
+	// mysql_connect("mysql5-23.bdb", "capbox001", "3Ty3FG0O");
+	// mysql_select_db("capbox001");
+?>
+<html>
+<head>
+</head>
+<body>
+<?php 
+?>
+</body>
+</html>
